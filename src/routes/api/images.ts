01652d06fcb7569334d0b30cad45a1a393475ac3
Andrea <56677245+andieissocool@users.noreply.github.com>
2024-11-08 20:16:57 +0100

    add correct path and sendFile

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -11,19 +11,11 @@ images.get('/', async (req, res) => {
   const response = await processImage(filename, width, height);
   const fullUrl = `${req.protocol}://${req.get('host')}/api/images/${filename}-thumb.jpg`;
 
-  res.send(response);
-
-  // /api/images?filename=fjord&width=200&height=200
-  // res.redirect(fullUrl);
-
-  // res.send(`
-  //   <html>
-  //     <body>
-  //       <h1>Processed Image</h1>
-  //       <img src="${fullUrl}" alt="Processed Image" />
-  //     </body>
-  //   </html>
-  // `);
+  const filePath = path.join(
+    __dirname,
+    `../../../assets/thumbs/${filename}-thumb.jpg`,
+  );
+  res.sendFile(filePath);
 });
 
 export default images;
